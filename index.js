8c8160f2cb77c3e1837d914de2b61c5c5a8e96f5
jackiejacobson <jjacobson336@gmail.com>
2022-08-15 01:20:15 -0700

    Extra styling/quiz format

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
 //Quiz Module
 const radios = document.querySelectorAll('input[type=radio][name="answer"]');
 
+console.log("radios is a type of:", typeof radios);
+console.log("radios is not an array");
+console.log("radios".radios);
+
 const radioMessages = {
-    answer1: "That’s not right",
-    answer2: "That’s right!",
-    answer3: "That's not right",
-    answer4: "That's not right"
+    answer1: {
+        correct: false,
+        message: "That's not right",
+    },
+    answer2: {
+        correct: true,
+        message: "That's right!",
+    },
+    answer3: {
+        correct: false,
+        message: "That's not right",
+    },
+    answer4: {
+        correct: false,
+        message: "That's not right"
+    }
 };
 
 radios.forEach((radio) => {
 
     radio.addEventListener("change", (e) => {
-        let selectedRadioButtonMessage = document.getElementById(
-            `${e.target.id}Message`
-        );
+        let selectedAnswerDiv = document.getElementById(e.target.id).parentNode;
+        console.log('selectedAnswerDiv', selectedAnswerDiv);
 
-        //Hide all messages
-        let allMessages = document.querySelectorAll(".message");
-        allMessages.forEach((message) => {
-            message.style.display = "none";
+        let answerResult = radioMessages[e.target.id].correct;
+        let correctClass = answerResult ? 'correct' : 'incorrect';
+
+        let allAnswerDivs = document.querySelectorAll("div.answer");
+        allAnswerDivs.forEach((answerDiv) => {
+            answerDiv.classList.remove('correct');
+            answerDiv.classList.remove('incorrect');
+            answerDiv.classList.remove('selected');
         });
 
-        let selectedRadioMessage = radioMessages[e.target.id];
+        let messageDivs = document.querySelectorAll(".message");
+        messageDivs.forEach((message) => {
+            message.innerHTML = '';
+        });
 
-        //show message
-        selectedRadioButtonMessage.style.display = "block";
-        selectedRadioButtonMessage.innerHTML = selectedRadioMessage;
+        selectedAnswerDiv.classList.add(`${correctClass}`);
+        selectedAnswerDiv.classList.add('selected');
+
+        let selectedRadioButtonMessageDiv = document.getElementById(
+            `${e.target.id}Message`
+        );
+        let selectedRadioMessage = radioMessages[e.target.id].message;
+        selectedRadioButtonMessageDiv.innerHTML = selectedRadioMessage;
     });
 });
 
