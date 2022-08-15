bd1a4e520b053ca11913b626b1e48da582e93868
jackiejacobson <jjacobson336@gmail.com>
2022-08-15 00:20:51 -0700

    Tidying up

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const radioMessages = {
 };
 
 radios.forEach((radio) => {
-    
+
     radio.addEventListener("change", (e) => {
         let selectedRadioButtonMessage = document.getElementById(
             `${e.target.id}Message`
         );
+
         //Hide all messages
         let allMessages = document.querySelectorAll(".message");
         allMessages.forEach((message) => {
@@ -21,28 +22,24 @@ radios.forEach((radio) => {
         });
 
         let selectedRadioMessage = radioMessages[e.target.id];
+
         //show message
         selectedRadioButtonMessage.style.display = "block";
         selectedRadioButtonMessage.innerHTML = selectedRadioMessage;
     });
 });
 
-
-
-
 //Tab Module
-function openCase(event, caseNum){
+function openCase(event, caseNum) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabs content");
-    for (i=0 ; i < tabcontent.length ; i++){
-        tabcontent[i].style.display="none";
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tabs links");
-    for (i=0; i< tablinks.length; i++){
-        tablinks[i].className = tablinks[i].className.replace(" active"
-        , "");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(caseNum).style.display = "flex";
     event.currentTarget.className += " active";
 }
-
